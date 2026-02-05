4e9d98975bc1ced8c2635c249c82eb8a07944314
GetAnshulAgarwal <anshulagarwal132@gmail.com>
2026-02-05 23:48:45 +0530

    Fix case sensitivity for utils folder

--- lib/screen/Support/support_tickets_screen.dart
+++ lib/screen/Support/support_tickets_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../authentication/user_data.dart';
-import '../../Utils/date_formatter.dart';
+import '../../utils/date_formatter.dart';
 import '../../model/Support/support_ticket.dart';
 import '../../services/Support/support_api_service.dart';
 import '../ProfileSubScreen/help_and_support_screen.dart';
